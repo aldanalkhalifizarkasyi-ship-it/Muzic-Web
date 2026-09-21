011b09b826b28e5d0daac805f5c01db45719fd58
Aldan Zarkasyi <aldanalkhalifizarkasyi@gmail.com>
2026-09-21 20:58:45 +0700

    Tambah lagu berharap tak berpisah

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 let mylove = document.getElementById("MyLoveSong");
 let myloveSong = new Audio("Songs/MyLove.mp3")
+let BerharapTakBerpisah = document.getElementById("BerharapTakBerpisahSong")
+let BerharapTakBerpisahSong = new Audio("Songs/berharaptakberpisah.mp3")
 
 mylove.addEventListener("click", function(){
     if (myloveSong.paused) {
-        mylove.setAttribute("src", "Images/pauseimg.png")
+        // mylove.setAttribute("src", "Images/pauseimg.png")
+        mylove.style.filter = "grayscale(70%)"
         myloveSong.play()
     } else {
-        mylove.setAttribute("src", "Images/playimg.png")
+        // mylove.setAttribute("src", "Images/playimg.png")
+        mylove.style.filter = "grayscale(0%)"
         myloveSong.pause();
     }
     // myloveSong.currentTime = 0
     // myloveSong.play();
+})
+
+BerharapTakBerpisah.addEventListener("click", function(){
+    if (BerharapTakBerpisahSong.paused) {
+        BerharapTakBerpisah.style.filter = "grayscale(70%)"
+        BerharapTakBerpisahSong.play()
+    } else {
+        BerharapTakBerpisah.style.filter = "grayscale(0%)"
+        BerharapTakBerpisahSong.pause()
+    }
 })
